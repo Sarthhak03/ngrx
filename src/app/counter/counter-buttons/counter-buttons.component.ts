cda7db963ef111d3338b9be33b05a908d8f3b95c
Sarthhak Kachwha <sarthhak.kachwha@taritas.com>
2024-04-19 01:11:51 +0530

    Skeleton of counter app

---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter-buttons.component.html',
+  styleUrl: './counter-buttons.component.css'
+})
+export class CounterButtonsComponent {
+onIncrement() {
+throw new Error('Method not implemented.');
+}
+onDecrement() {
+throw new Error('Method not implemented.');
+}
+onReset() {
+throw new Error('Method not implemented.');
+}
+
+}
